623a502649794abe779159d2a7647bbc7f3745b8
ElMariuso <thiry.marius@gmail.com>
2022-08-16 15:09:40 +0200

    Huge merge 2

--- cub_utils_1.c
+++ cub_utils_1.c
@@ -64,21 +64,7 @@ char	*cub_strndup(char const *str, size_t size, t_data *data)
 	return (dest);
 }
 
-<<<<<<< HEAD
-int	ft_strcmp(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i] && s2[i] && (s1[i] == s2[i]))
-		++i;
-	return (s1[i] - s2[i]);
-}
-
-void	*cub_malloc(size_t n, t_data *data)
-=======
 void	*cub_malloc(size_t n, t_input *data)
->>>>>>> parsing
 {
 	void	*ptr;
 
