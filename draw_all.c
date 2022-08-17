7725968a5c02365d66cc93b189344823b4de99b1
ElMariuso <thiry.marius@gmail.com>
2022-08-17 14:06:12 +0200

    Shorter function

--- draw_all.c
+++ draw_all.c
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 18:25:50 by mthiry            #+#    #+#             */
-/*   Updated: 2022/08/17 13:58:41 by mthiry           ###   ########.fr       */
+/*   Updated: 2022/08/17 14:05:53 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,28 @@ void    draw_square(t_img img, int color, int end_i, int end_j)
     }
 }
 
+void    init_background(t_data *data)
+{
+    int color;
+    
+    data->img.addr = mlx_get_data_addr(data->img.img_ptr, &data->img.bpp,
+		&data->img.line_length, &data->img.endian);
+    color = create_trgb(0, ft_atoi(data->map.c_spl[0]), ft_atoi(data->map.c_spl[1]), ft_atoi(data->map.c_spl[2]));
+    draw_square(data->img, color, HEIGHT, WIDTH);
+    mlx_put_image_to_window(data->mlx, data->win, data->img.img_ptr, 0, 0);
+}
+
+void    init_player(t_data *data)
+{
+    int color;
+
+    data->player.addr = mlx_get_data_addr(data->player.img_ptr, &data->player.bpp,
+		&data->player.line_length, &data->player.endian);
+    color = 0X000000;
+    draw_square(data->player, color, 10, 10);
+    mlx_put_image_to_window(data->mlx, data->win, data->player.img_ptr, data->pos_x, data->pos_y);
+}
+
 // void    draw_map(t_data *data)
 // {
 //     int x0;
@@ -41,28 +63,14 @@ void    draw_square(t_img img, int color, int end_i, int end_j)
 
 void    draw_all(t_data *data)
 {
-    int color;
-    
     data->img.img_ptr = mlx_new_image(data->mlx, WIDTH, HEIGHT);
     if (data->img.img_ptr != NULL)
-    {
-        data->img.addr = mlx_get_data_addr(data->img.img_ptr, &data->img.bpp,
-		    &data->img.line_length, &data->img.endian);
-        color = create_trgb(0, ft_atoi(data->map.c_spl[0]), ft_atoi(data->map.c_spl[1]), ft_atoi(data->map.c_spl[2]));
-        draw_square(data->img, color, HEIGHT, WIDTH);
-        mlx_put_image_to_window(data->mlx, data->win, data->img.img_ptr, 0, 0);
-    }
+        init_background(data);
     //else
         // put error here
     data->player.img_ptr = mlx_new_image(data->mlx, 10, 10);
     if (data->player.img_ptr != NULL)
-    {
-        data->player.addr = mlx_get_data_addr(data->player.img_ptr, &data->player.bpp,
-		    &data->player.line_length, &data->player.endian);
-        color = 0X000000;
-        draw_square(data->player, color, 10, 10);
-        mlx_put_image_to_window(data->mlx, data->win, data->player.img_ptr, data->pos_x, data->pos_y);
-    }
+        init_player(data);
     //else
         // put error here
 }
