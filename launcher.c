24426dd8a1f50563e99ac358a00b63ff58ed91e0
ElMariuso <thiry.marius@gmail.com>
2022-08-18 18:43:07 +0200

    Change some values

--- launcher.c
+++ launcher.c
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 13:11:23 by mthiry            #+#    #+#             */
-/*   Updated: 2022/08/18 18:24:02 by mthiry           ###   ########.fr       */
+/*   Updated: 2022/08/18 18:42:36 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,11 @@ void    init_mlx_and_window(t_data *data)
     check_mlx(data->mlx, data);
     data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "cub3d");
     check_win(data);
-    data->pos_x_minimap = (WIDTH / 2) - ((data->map.width * 50) / 2);
-    data->pos_y_minimap = (HEIGHT / 2) - ((data->map.height * 50) / 2);
+    // data->pos_x_minimap = (WIDTH / 2) - ((data->map.width * 50) / 2);
+    // data->pos_y_minimap = (HEIGHT / 2) - ((data->map.height * 50) / 2);
+    data->pos_x_minimap = 10;
+    data->pos_y_minimap = 10;
+
     data->size_player = 11;
     data->size_square = 55;
     data->player_s.p_ang = 0;
