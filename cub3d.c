6428ff7316c90520bc09fdedf0903844becfd54c
baltsaros <abuzdin@student.s19.be>
2022-09-07 12:36:14 +0200

    clean-up

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,6 @@ int	main(int argc, char *argv[])
 	
 	if (argc != 2)
 		error_exit(&data, "Ivalid amount of arguments", 0);
-	// data.angle = data.map.angle % 360 - 30;
 	init_map(&data, argv[1]);
 	data.mlx = mlx_init();
 	check_mlx(data.mlx, &data);
@@ -19,22 +18,6 @@ int	main(int argc, char *argv[])
 	data.pcoord[1] = data.py;
 	data.px = data.px * data.sx;
 	data.py = data.py * data.sy;
-	// if (data.map.dir == 'N')
-	// 	data.pa = (90 * PI) / 180;
-	// else if (data.map.dir == 'E')
-	// 	data.pa = (0 * PI) / 180;
-	// else if (data.map.dir == 'W')
-	// 	data.pa = (180 * PI) / 180;
-	// else if (data.map.dir == 'S')
-	// 	data.pa = (270 * PI) / 180;
-	// data.ra = (60 * PI) / 180;
-	// data.rdx = cos(data.ra);
-	// data.rdy = sin(data.ra);
-	// data.la = (120 * PI) / 180;
-	// data.ldx = cos(data.la);
-	// data.ldy = sin(data.la);
-	// data.pdx = cos(data.pa);
-	// data.pdy = sin(data.pa);
 	data.psize = 8;
 	data.pl.mlx_img = mlx_new_image(data.mlx, data.psize, data.psize);
 	data.pl.addr = mlx_get_data_addr(data.pl.mlx_img, &data.pl.bpp,
