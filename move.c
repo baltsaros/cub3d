a7b76bb1127d9bda81e1ca1315866b9d9432a5ba
ElMariuso <thiry.marius@gmail.com>
2022-11-15 16:56:39 +0100

    Better left and right movement

--- move.c
+++ move.c
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 15:15:12 by mthiry            #+#    #+#             */
-/*   Updated: 2022/11/15 16:09:02 by mthiry           ###   ########.fr       */
+/*   Updated: 2022/11/15 16:56:00 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,33 +55,23 @@ void    move_down(t_data *data, t_ray_calcul *collisions)
 void    move_right(t_data *data, t_ray_calcul *collisions)
 {
     (void)collisions;
-    float   new_ang;
-    float   delta_y_right;
-    float   delta_x_right;
-    
-    new_ang = FixAng(data->player_s.p_ang + 90);
-    delta_x_right = cos(degToRad(new_ang));
-    delta_y_right = -sin(degToRad(new_ang));
-    data->player_s.pos_win_y -= data->player_s.speed * (delta_y_right / 2);
-    data->player_s.pos_win_x -= data->player_s.speed * (delta_x_right / 2);
-    data->player_s.pos_y -= data->player_s.speed * delta_y_right;
-    data->player_s.pos_x -= data->player_s.speed * delta_x_right;
+
+    data->player_s.pos_win_x -= data->player_s.speed * (data->player_s.delta_y / 2);
+    data->player_s.pos_x -= data->player_s.speed * data->player_s.delta_y;
+
+    data->player_s.pos_win_y -= data->player_s.speed * (-data->player_s.delta_x / 2);
+    data->player_s.pos_y -= data->player_s.speed * -data->player_s.delta_x;
 }
 
 void    move_left(t_data *data, t_ray_calcul *collisions)
 {
     (void)collisions;
-    float   new_ang;
-    float   delta_y_right;
-    float   delta_x_right;
     
-    new_ang = FixAng(data->player_s.p_ang - 90);
-    delta_x_right = cos(degToRad(new_ang));
-    delta_y_right = -sin(degToRad(new_ang));
-    data->player_s.pos_win_y -= data->player_s.speed * (delta_y_right / 2);
-    data->player_s.pos_win_x -= data->player_s.speed * (delta_x_right / 2);
-    data->player_s.pos_y -= data->player_s.speed * delta_y_right;
-    data->player_s.pos_x -= data->player_s.speed * delta_x_right;
+    data->player_s.pos_win_x -= data->player_s.speed * (-data->player_s.delta_y / 2);
+    data->player_s.pos_x -= data->player_s.speed * -data->player_s.delta_y;
+
+    data->player_s.pos_win_y -= data->player_s.speed * (data->player_s.delta_x / 2);
+    data->player_s.pos_y -= data->player_s.speed * data->player_s.delta_x;
 }
 
 void    move(t_data *data)
