88cc8f4a3beff40880dfde5a6645639e191330d4
ElMariuso <thiry.marius@gmail.com>
2022-11-15 16:06:32 +0100

    tmp collisions desactivation

--- move.c
+++ move.c
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 15:15:12 by mthiry            #+#    #+#             */
-/*   Updated: 2022/11/15 15:12:13 by mthiry           ###   ########.fr       */
+/*   Updated: 2022/11/15 16:05:37 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,42 @@
 
 void    move_up(t_data *data, t_ray_calcul *collisions)
 {
-    t_point col_x;
-    t_point col_y;
+    (void)collisions;
+    // t_point col_x;
+    // t_point col_y;
 
-    col_x.y = (collisions->ipy * data->map.width + collisions->ipx_add_xo) / data->map.height;
-    col_x.x = collisions->ipy * data->map.width + collisions->ipx_add_xo;
-    printf("col_x.y: %d\n", col_x.y);
-    printf("col_y.x: %d\n", col_x.x);
+    // col_x.y = (collisions->ipy * data->map.width + collisions->ipx_add_xo) / data->map.height;
+    // col_x.x = collisions->ipy * data->map.width + collisions->ipx_add_xo;
+    // printf("col_x.y: %d\n", col_x.y);
+    // printf("col_y.x: %d\n", col_x.x);
 
-    if (data->map.map[col_x.y][col_x.x] == '0')
-    {
+    // if (data->map.map[col_x.y][col_x.x] == '0')
+    // {
         data->player_s.pos_win_x += data->player_s.speed * (data->player_s.delta_x / 2);
         data->player_s.pos_x += data->player_s.speed * data->player_s.delta_x;
-    }
-    col_y.y = (collisions->ipy_add_yo * data->map.width + collisions->ipx) / data->map.height;
-    col_y.x = 0;
-    if (data->map.map[col_y.y][col_y.x] == '0')
-    {
+    // }
+    // col_y.y = (collisions->ipy_add_yo * data->map.width + collisions->ipx) / data->map.height;
+    // col_y.x = 0;
+    // if (data->map.map[col_y.y][col_y.x] == '0')
+    // {
         data->player_s.pos_win_y += data->player_s.speed * (data->player_s.delta_y / 2);
 	    data->player_s.pos_y += data->player_s.speed * data->player_s.delta_y;
-    }
+    // }
 }
 
 void    move_down(t_data *data, t_ray_calcul *collisions)
 {
-    if (data->map.map[collisions->ipy * data->map.width + collisions->ipx_sub_xo] == 0)
-    {
+    (void)collisions;
+    // if (data->map.map[collisions->ipy * data->map.width + collisions->ipx_sub_xo] == 0)
+    // {
         data->player_s.pos_win_x -= data->player_s.speed * (data->player_s.delta_x / 2);
         data->player_s.pos_x -= data->player_s.speed * data->player_s.delta_x;
-    }
-    if (data->map.map[collisions->ipy_sub_yo * data->map.width + collisions->ipx] == 0)
-    {
+    // }
+    // if (data->map.map[collisions->ipy_sub_yo * data->map.width + collisions->ipx] == 0)
+    // {
         data->player_s.pos_win_y -= data->player_s.speed * (data->player_s.delta_y / 2);
 	    data->player_s.pos_y -= data->player_s.speed * data->player_s.delta_y;
-    }
+    // }
 }
 
 void    move_right(t_data *data, t_ray_calcul *collisions)
