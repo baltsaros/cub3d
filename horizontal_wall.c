b819f2c03ce871a83c835e3ce845939470ca54a7
ElMariuso <thiry.marius@gmail.com>
2022-11-17 17:53:57 +0100

    Horizontal_wall.c norminette

--- horizontal_wall.c
+++ horizontal_wall.c
@@ -6,66 +6,68 @@
 /*   By: abuzdin <abuzdin@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 16:36:36 by mthiry            #+#    #+#             */
-/*   Updated: 2022/11/08 18:32:06 by mthiry           ###   ########.fr       */
+/*   Updated: 2022/11/17 17:53:43 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-float	disH_calcul(t_data *data, float ra, float ry, float rx)
+float	dish_calcul(t_data *data, float ra, float ry, float rx)
 {
-	float	disH;
+	float	dish;
 	float	first;
 	float	second;
 
 	first = cos(degToRad(ra)) * (rx - data->player_s.pos_x);
 	second = sin(degToRad(ra)) * (ry - data->player_s.pos_y);
-	disH = first - second;
-	return (disH);
+	dish = first - second;
+	return (dish);
 }
 
 void	calculate_horizontal_distance(t_data *data, t_ray_calcul *ray, int dof)
 {
-    while ((size_t)dof < data->map.height)
-    {
-        ray->mx = ((int)(ray->rx) / SQUARE_SIZE);
-        ray->my = ((int)(ray->ry) / SQUARE_SIZE);
-        if (ray->my >= 0 && ray->mx >= 0
-            && ray->my < (int)data->map.height
-            && ray->mx < (int)data->map.width
-            && data->map.map[ray->my][ray->mx] == '1')
-        {
-            dof = data->map.height;
-            ray->disH = disH_calcul(data, ray->ra, ray->ry, ray->rx);
-        }
-        else
-        {
-            ray->rx += ray->xo;
-            ray->ry += ray->yo;
-            dof++;
-        }
-    }
+	while ((size_t)dof < data->map.height)
+	{
+		ray->mx = ((int)(ray->rx) / SQUARE_SIZE);
+		ray->my = ((int)(ray->ry) / SQUARE_SIZE);
+		if (ray->my >= 0 && ray->mx >= 0
+			&& ray->my < (int)data->map.height
+			&& ray->mx < (int)data->map.width
+			&& data->map.map[ray->my][ray->mx] == '1')
+		{
+			dof = data->map.height;
+			ray->disH = disH_calcul(data, ray->ra, ray->ry, ray->rx);
+		}
+		else
+		{
+			ray->rx += ray->xo;
+			ray->ry += ray->yo;
+			dof++;
+		}
+	}
 }
 
-int		check_up(t_data *data, t_ray_calcul *ray, float Tan)
+int	check_up(t_data *data, t_ray_calcul *ray, float Tan)
 {
-	ray->ry = (((int)data->player_s.pos_y / SQUARE_SIZE) * SQUARE_SIZE) - 0.0001;
+	ray->ry = (((int)data->player_s.pos_y / SQUARE_SIZE) * SQUARE_SIZE)
+		- 0.0001;
 	ray->rx = (data->player_s.pos_y - ray->ry) * Tan + data->player_s.pos_x;
 	ray->yo = -SQUARE_SIZE;
 	ray->xo = -ray->yo * Tan;
 	return (NORTH);
 }
 
-int		check_down(t_data *data, t_ray_calcul *ray, float Tan)
+int	check_down(t_data *data, t_ray_calcul *ray, float Tan)
 {
-	ray->ry = (((int)data->player_s.pos_y / SQUARE_SIZE) * SQUARE_SIZE) + SQUARE_SIZE;
+	ray->ry = (((int)data->player_s.pos_y / SQUARE_SIZE) * SQUARE_SIZE)
+		+ SQUARE_SIZE;
 	ray->rx = (data->player_s.pos_y - ray->ry) * Tan + data->player_s.pos_x;
 	ray->yo = SQUARE_SIZE;
 	ray->xo = -ray->yo * Tan;
 	return (SOUTH);
 }
 
-int check_horizontal_wall(t_data *data, t_ray_calcul *ray, float Tan)
+int	check_horizontal_wall(t_data *data, t_ray_calcul *ray, float Tan)
 {
 	int	dof;
 	int	ret;
