79a8448456c1fb2d4e78ee0f19ed8cf562e565c7
ElMariuso <thiry.marius@gmail.com>
2022-11-17 17:37:44 +0100

    minimap norminette

--- minimap.c
+++ minimap.c
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 16:49:59 by mthiry            #+#    #+#             */
-/*   Updated: 2022/11/17 16:52:58 by mthiry           ###   ########.fr       */
+/*   Updated: 2022/11/17 17:37:30 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,42 +17,42 @@ void	draw_square_coord(t_data *data, int color, int x, int y)
 	int	i;
 	int	j;
 
-    i = 0;
-    while (i != SQUARE_SIZE / 2)
-    {
-        j = 0;
-        while (j != SQUARE_SIZE / 2)
-        {
-            mlx_pixel_put_img(&data->minimap, x + j, y + i, color);
-            j++;
-        }
-        i++;
-    }
+	i = 0;
+	while (i != SQUARE_SIZE / 2)
+	{
+		j = 0;
+		while (j != SQUARE_SIZE / 2)
+		{
+			mlx_pixel_put_img(&data->minimap, x + j, y + i, color);
+			j++;
+		}
+		i++;
+	}
 }
 
 void	draw_map(t_data *data, int color, int height, int width)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (i != height)
-    {
-        j = 0;
-        while (j != width)
-        {
-            if (data->map.map[i][j] == '1')
-                draw_square_coord(data, color, j * (SQUARE_SIZE / 2), i * (SQUARE_SIZE / 2));
-            j++;
-        }
-        i++;
-    }
+	i = 0;
+	while (i != height)
+	{
+		j = 0;
+		while (j != width)
+		{
+			if (data->map.map[i][j] == '1')
+				draw_square_coord(data, color, j * (SQUARE_SIZE / 2), i * (SQUARE_SIZE / 2));
+			j++;
+		}
+		i++;
+	}
 }
 
-void init_minimap_values(t_data *data)
+void	init_minimap_values(t_data *data)
 {
-    data->minimap_s.width = data->map.width * (SQUARE_SIZE / 2);
-    data->minimap_s.height = data->map.height * (SQUARE_SIZE / 2);
-    data->minimap_s.position.x = (WIDTH / 2) - (data->minimap_s.width / 2);
-    data->minimap_s.position.y = 0;
+	data->minimap_s.width = data->map.width * (SQUARE_SIZE / 2);
+	data->minimap_s.height = data->map.height * (SQUARE_SIZE / 2);
+	data->minimap_s.position.x = (WIDTH / 2) - (data->minimap_s.width / 2);
+	data->minimap_s.position.y = 0;
 }
