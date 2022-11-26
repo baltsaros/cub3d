1ae74804ac37b1696c53cf1e9d15bf89b6e62d61
baltsaros <buzdin.alexander@gmail.com>
2022-11-26 15:17:48 +0100

    comment to change speed value

--- player.c
+++ player.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   player.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abuzdin <abuzdin@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 16:16:23 by mthiry            #+#    #+#             */
-/*   Updated: 2022/11/26 01:42:01 by mthiry           ###   ########.fr       */
+/*   Updated: 2022/11/26 15:17:33 by abuzdin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	is_player(char c)
 
 void	init_player_values(t_data *data)
 {
+	// float	x;
+
 	data->player_s.pos_x = 0;
 	data->player_s.pos_y = 0;
 	data->player_s.p_ang = data->map.angle;
@@ -31,6 +33,10 @@ void	init_player_values(t_data *data)
 	data->player_s.pos_x = (data->map.coord[0] * SQUARE_SIZE)
 		+ (SQUARE_SIZE / 2) - (PSIZE / 2);
 	data->player_s.speed = 10;
+	// x = (data->player_s.speed * 100) / SQUARE_SIZE;
+	// printf("step: %d\n", data->player_s.speed);
+	// data->minimap_s.step = (PSIZE * x) / 100;
+	// printf("x:%f\nmspeed: %f\n", x, data->minimap_s.step);
 	data->player_s.rot_speed = 5;
 	data->player_s.pos_win_x = data->minimap_s.width / 2 - PSIZE / 2;
 	data->player_s.pos_win_y = data->minimap_s.height / 2 - PSIZE / 2;
